6ac9a07da2d5d0bbed00713c02b241fcb1b18696
java <java@bigdata>
2017-09-05 17:50:44 +0900

    xxx

--- src/main/java/com/example/HelloSpring.java
+++ src/main/java/com/example/HelloSpring.java
@@ -13,6 +13,11 @@ public class HelloSpring {
 //		System.out.println("1. DI(Dependency Injection) 의존 주입");
 //		System.out.println("   - IoC의 Spring이 구현한 구체적인 구현");
 //		System.out.println("2. AOP(Aspect Objected Programming 관점 지향 프로그래밍");
+
+//		System.out.println("Spring의 두가지 추상화된 설계");
+//		System.out.println("1. DI(Dependency Injection) 의존 주입");
+//		System.out.println("   - IoC의 Spring이 구현한 구체적인 구현");
+//		System.out.println("2. AOP(Aspect Objected Programming 관점 지향 프로그래밍");
 	}
 
 }
